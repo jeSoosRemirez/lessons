4b055b51acdaa5356d58dd3a6dd63b8c8b913ab0
jeSoosRemirez <dkhomych0091@gmail.com>
2018-07-03 14:11:11 +0300

    99% first task!!!AAAAAAA

--- 1_lesson/1_task_improved.py
+++ 1_lesson/1_task_improved.py
@@ -5,14 +5,19 @@ clock_minutes = int(input('А хвилин: '))
 hours = (want_sleep_hours + clock_hours)
 minutes = (want_sleep_minutes + clock_minutes)
 
-if hours >= 23:
-    alarm_hours = (clock_hours + 1)
-    alarm_minutes = (60 - minutes)
-    if minutes > 60:
-        if minutes == 120:
-            alarm_hours = (want_sleep_hours + 1)
-        elif minutes == 60:
-            alarm_hours = want_sleep_hours
-else:
-    print('Чому так пізно!')
-print(f'{alarm_hours} год і {alarm_minutes} хв')
+alarm_hours = 0
+alarm_minutes = 0
+
+if clock_hours == 23:
+    alarm_hours = want_sleep_hours - 1
+elif hours > 23:
+    alarm_hours = hours - 24
+elif minutes < 60:
+    alarm_minutes = minutes
+elif minutes > 60:
+    alarm_hours = alarm_hours + 1
+    alarm_minutes = minutes - 60
+elif minutes == 60:
+    alarm_hours = alarm_hours + 1
+    alarm_minutes = 0
+print(f'Наведіть годинник на {alarm_hours} год і {alarm_minutes} хв')
