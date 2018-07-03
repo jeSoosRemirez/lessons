bd846152d428355279625242a58c65d610dbbac8
jeSoosRemirez <dkhomych0091@gmail.com>
2018-07-03 16:03:53 +0300

    All tasks up to third lesson finished

--- 1_lesson/2_task.py
+++ 1_lesson/2_task.py
@@ -1,13 +1,18 @@
 sleep_hours = int(input('Скільки годин ви спали сьогодні(лише години!): '))
 sleep_minutes = int(input('А хвилин: '))
 sleep = (sleep_hours * 60 + sleep_minutes)
-if sleep == sleep in range(450, 500):
-    print(f'Ви гарно виспалися!')
-elif sleep < 450:
-    need_sleep_hours = (60 * (7 - sleep_hours))
-    minutes = (need_sleep_hours - sleep_minutes)
-    print(f'Ви недоспали {minutes} хвилин!')
-elif sleep > 500:
-    need_sleep_hours = (60 * (8 - sleep_hours))
-    minutes = (need_sleep_hours + sleep_minutes)
-    print(f'Ви перспали на {minutes} хвилини!')
+
+if (sleep_hours == 7 and sleep_minutes >= 20) or (sleep_hours == 8 and sleep_minutes <= 20):
+    print('Ви гарно виспались')
+elif sleep_hours < 7:
+    need_sleep_m = ((7 - sleep_hours) * 60 - sleep_minutes)
+    print(f'Ви недоспали {need_sleep_m} хв')
+elif sleep_hours > 8:
+    need_sleep_m = ((sleep_hours - 8) * 60 - sleep_minutes)
+    print(f'Ви переспали {need_sleep_m} хв')
+elif sleep_hours == 7 and sleep_minutes < 20:
+    need_sleep_m = 20 - sleep_minutes
+    print(f'Ви недоспали {need_sleep_m} хв')
+elif sleep_hours == 8 and sleep_minutes > 20:
+    need_sleep_m = sleep_minutes - 20
+    print(f'Ви переспали {need_sleep_m} хв')
